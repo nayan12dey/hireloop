2685e86a9d2c1d4a0795e0b385adb837b1028ba7
Nayan Dey <ndeynayan123@gmail.com>
2026-06-15 17:43:12 +0530

    added dashboard layout, sidebar toggle and reusable dashboard stats

---
+++ src/app/dashboard/recruiter/jobs/page.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const RecruiterJobs = () => {
+    return (
+        <div>
+            <h2>Recruiter/company Manage All Jobs</h2>
+        </div>
+    );
+};
+
+export default RecruiterJobs;
